8d4ed2f2123c4644c3a9b7386247fe0d8d6009b0
Bartek Siedlecki <bsiedlecki3474@gmail.com>
2022-05-11 21:17:47 +0200

    refactor: Db - update init sql

--- init.sql
+++ init.sql
@@ -1,16 +1,6 @@
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`weexam` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `weexam`;
 
 USE `weexam`;
-
-/*Table structure for table `wee_groups` */
 
 DROP TABLE IF EXISTS `wee_groups`;
 
@@ -27,9 +17,11 @@ CREATE TABLE `wee_groups` (
   KEY `fk_wee_groups_modified_by` (`modified_by`),
   CONSTRAINT `fk_wee_groups_created_by` FOREIGN KEY (`created_by`) REFERENCES `wee_users` (`id`) ON DELETE SET NULL,
   CONSTRAINT `fk_wee_groups_modified_by` FOREIGN KEY (`modified_by`) REFERENCES `wee_users` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET UTF8;
 
-/*Table structure for table `wee_groups_users` */
+INSERT INTO `wee_groups` (`id`,`name`,`is_active`,`created_by`,`created_on`) VALUES 
+(1,'IZ07IO2',1,1,'2022-01-09 12:45:37'),
+(2,'IZ07TC1',1,1,'2022-01-09 12:45:46');
 
 DROP TABLE IF EXISTS `wee_groups_users`;
 
@@ -40,9 +32,10 @@ CREATE TABLE `wee_groups_users` (
   KEY `fk_wee_groups_users_user_id` (`user_id`),
   CONSTRAINT `fk_wee_groups_users_group_id` FOREIGN KEY (`group_id`) REFERENCES `wee_groups` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_wee_groups_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `wee_users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET UTF8;
 
-/*Table structure for table `wee_users` */
+INSERT INTO `wee_groups_users` (`group_id`,`user_id`) VALUES 
+(1,2);
 
 DROP TABLE IF EXISTS `wee_users`;
 
@@ -59,21 +52,19 @@ CREATE TABLE `wee_users` (
   `modified_on` datetime DEFAULT NULL,
   `modified_by` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET UTF8;
 
-/*Data for the table `wee_users` */
-
-insert  into `wee_users`(`id`,`username`,`password`,`first_name`,`last_name`,`role`,`is_active`,`created_on`,`created_by`,`modified_on`,`modified_by`) values 
-(1,'root','$2a$10$8dGOKOFbmSfGMvPMFBvcleaYcqvykdTYqCoFnXDFd8vIdpXmv0nvG','root','root','root',1,'2021-12-17 23:22:49',1,NULL,NULL);
+INSERT INTO `wee_users` (`id`,`username`,`password`,`first_name`,`last_name`,`role`,`is_active`,`created_on`,`created_by`,`modified_on`,`modified_by`) VALUES 
+(1,'root','$2a$10$8dGOKOFbmSfGMvPMFBvcleaYcqvykdTYqCoFnXDFd8vIdpXmv0nvG','root','root','root',1,'2021-12-17 23:22:49',1,NULL,NULL),
+(2,'user1','$2a$10$/fPYvPTHfxIddg/1.4ZoJOWhtHvw.yIC/a8icIANvliJSuKttlyjy','janusz','testowy','user',1,'2021-12-17 23:37:41',1,NULL,NULL),
+(3,'teacher1','$2a$10$np9n5g75bVEHZiEvK24aMOTXoKIHvnEdbZPopgx1P.JCbcroWq2D2','profesor','testowy','admin',1,'2021-12-20 01:52:45',1,NULL,NULL),
+(4,'asd','$2a$10$UkesnosovZaErzKJw.UDruJiNKUKZtEVzcTGPE2/sB0dPxK1gTYEO','asd','asd','user',0,'2022-01-30 00:04:26',1,NULL,NULL);
 
 DROP TABLE IF EXISTS `wee_tests`;
 
 CREATE TABLE `wee_tests` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `end_date` datetime DEFAULT NULL,
-  `duration` int unsigned default null,
   `show_scores` tinyint unsigned NOT NULL DEFAULT '1',
   `is_active` tinyint unsigned NOT NULL DEFAULT '0',
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -81,20 +72,108 @@ CREATE TABLE `wee_tests` (
   `modified_on` datetime DEFAULT NULL,
   `modified_by` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) DEFAULT CHARSET UTF8;
 
-DROP TABLE IF EXISTS `wee_tests_groups`;
+INSERT INTO `wee_tests` (`id`, `name`, `show_scores`, `is_active`, `created_on`, `created_by`) VALUES
+(1, 'Test z BSK', 1, 1, '2022-02-09 20:16:28', 1),
+(2, 'Test 2', 0, 0, '2022-02-18 17:43:58', 1);
 
-CREATE TABLE `wee_tests_groups` (
-  `test_id` int unsigned NOT NULL,
+DROP TABLE IF EXISTS `wee_tests_events`;
+
+CREATE TABLE `wee_tests_events` (  
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `test_id` INT UNSIGNED,
+  `start_date` DATETIME,
+  `end_date` DATETIME,
+  `duration` INT UNSIGNED,
+  `is_active` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  CONSTRAINT `fk_wee_tests_events_test_id` FOREIGN KEY (`test_id`) REFERENCES `wee_tests` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
+
+DROP TABLE IF EXISTS `wee_events_groups`;
+
+CREATE TABLE `wee_events_groups` (
+  `event_id` int unsigned NOT NULL,
   `group_id` int unsigned NOT NULL,
-  PRIMARY KEY (`test_id`,`group_id`),
-  KEY `fk_wee_tests_groups_test_id` (`test_id`),
-  CONSTRAINT `fk_wee_tests_groups_test_id` FOREIGN KEY (`test_id`) REFERENCES `wee_tests` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_wee_tests_groups_group_id` FOREIGN KEY (`group_id`) REFERENCES `wee_groups` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  PRIMARY KEY (`event_id`,`group_id`),
+  CONSTRAINT `fk_wee_events_groups_event_id` FOREIGN KEY (`event_id`) REFERENCES `wee_tests_events` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_wee_events_groups_group_id` FOREIGN KEY (`group_id`) REFERENCES `wee_groups` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+INSERT INTO `wee_tests_events` (`id`, `test_id`, `start_date`, `end_date`, `duration`) VALUES
+(1, 1, '2022-03-06 11:00:00', '2022-03-06 12:00:00', 30),
+(2, 1, '2022-03-13 09:00:00', '2022-03-13 10:00:00', 30);
+
+DROP TABLE IF EXISTS `wee_questions_answer_types`;
+
+CREATE TABLE `wee_questions_answer_types` (  
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(20)
+) DEFAULT CHARSET UTF8;
+
+INSERT INTO `wee_questions_answer_types` (`id`, `name`) VALUES
+(1, 'Single choice'),
+(2, 'Multiple choice');
+
+
+DROP TABLE IF EXISTS `wee_tests_questions`;
+DROP TABLE IF EXISTS `wee_questions_answers`;
+
+CREATE TABLE `wee_questions` (  
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `content` TEXT NOT NULL,
+  `answer_type_id` INT UNSIGNED NOT NULL,
+  CONSTRAINT `fk_wee_questions_answer_type_id` FOREIGN KEY (`answer_type_id`) REFERENCES `wee_questions_answer_types` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
+
+
+CREATE TABLE `wee_tests_questions` (
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `test_id` INT UNSIGNED NOT NULL,
+  `content` TEXT NOT NULL,
+  `answer_type_id` INT UNSIGNED NOT NULL,
+  KEY `fk_wee_tests_questions_test_id` (`test_id`),
+  CONSTRAINT `fk_wee_tests_questions_test_id` FOREIGN KEY (`test_id`) REFERENCES `wee_tests` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
+
+CREATE TABLE `wee_questions_answers` (
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `question_id` INT UNSIGNED NOT NULL,
+  `value` TEXT NOT NULL,
+  `checked` TINYINT(1) NOT NULL,
+  KEY `fk_wee_questions_answers_question_id` (`question_id`),
+  CONSTRAINT `fk_wee_questions_answers_question_id` FOREIGN KEY (`question_id`) REFERENCES `wee_tests_questions` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
+
+INSERT INTO `wee_tests_questions` (`id`, `test_id`, `content`, `answer_type_id`) VALUES
+(1, 1, "Kiedy odkryto amerykę?", 1),
+(2, 1, "24=", 2);
+
+INSERT INTO `wee_questions_answers` (`id`, `question_id`, `value`, `checked`) VALUES
+(1, 1, 1491, 0),
+(2, 1, 1492, 1),
+(3, 1, 1493, 0),
+(4, 1, 1494, 0),
+(5, 2, "12+12", 1),
+(6, 2, "sqrt(484)", 0),
+(7, 2, "6*4", 1),
+(8, 2, "28-7", 0),
+(9, 2, "-24+48", 1);
+
+DROP TABLE IF EXISTS `wee_tests_assessments`;
+
+CREATE TABLE `wee_tests_assessments` (  
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `event_id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `start_date` DATETIME,
+  `end_date` DATETIME,
+  `is_active` TINYINT(1) UNSIGNED NOT NULL,
+  `created_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` INT UNSIGNED NOT NULL,
+  CONSTRAINT `fk_wee_tests_assessments_event_id` FOREIGN KEY (`event_id`) REFERENCES `wee_tests_events` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_wee_tests_assessments_user_id` FOREIGN KEY (`user_id`) REFERENCES `wee_users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_wee_tests_assessments_created_by` FOREIGN KEY (`created_by`) REFERENCES `wee_users` (`id`) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8;
+
